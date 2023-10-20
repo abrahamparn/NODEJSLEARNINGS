c1d8d267586681e1dc3e329b0559cafe3219bced
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2023-10-20 23:31:18 +0700

    adding hbs and creating shared layout (like .net i guess?)

--- EXPRESS-PROJECT/controllers/messages.controllers.js
+++ EXPRESS-PROJECT/controllers/messages.controllers.js
@@ -1,7 +1,10 @@
 const path = require('path')
 function getMessages(req, res){
     
-    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'testing.jpg'));
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'testing.jpg'));
+    res.render('messages', {
+        friend: "TRJ"
+    });
     // res.send('<ul><li>Abraham Naiborhu</li></ul>');
 }
 
--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -4,16 +4,27 @@ const friendsRouter = require('./router/friends.router')
 const messagesRouter = require('./router/messages.router')
 const listOfFriendsRouter = require('./router/listOfFriends.router')
 
+const path = require('path')
 
 const app = express();
+// to set view engine bust be under express
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 const PORT = 3000;
-const path = require('path')
 
 app.use((req, res, next)=>{
     const start = Date.now()
     next()
     const end = Date.now() - start
     console.log(`${req.method} and ${req.baseUrl}${req.url} and ${end}ms`)
+})
+// creating root source
+app.get('/', (req, res)=>{
+    res.render('index', {
+        title: 'This is using hbs',
+        caption: 'MANTAPPPPPPPP',
+        alt:"postman"
+    })
 })
 
 app.use('/site', express.static(path.join(__dirname, 'public')))
